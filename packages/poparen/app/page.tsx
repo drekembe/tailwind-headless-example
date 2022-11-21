27fae78fc36f9f009cf644cc6372dd981e522631
Miran Lipovaca <m.lipovaca@cargox.io>
2022-11-21 15:59:53 +0100

    OK

--- packages/poparen/app/page.tsx
+++ packages/poparen/app/page.tsx
@@ -1,5 +1,13 @@
+import { cx } from 'class-variance-authority';
 import { Button } from '@supa/components/button/button';
 export default function Page() {
+  const classes = cx([
+    'transform transition-all absolute top-0',
+    'left-0 h-full flex items-center pl-2 text-sm group-focus-within:text-xs',
+    'peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2',
+    'group-focus-within:-translate-y-full',
+    'peer-valid:-translate-y-full group-focus-within:pl-0 peer-valid:pl-0',
+  ]);
   return (
     <div className="compact">
       <div className="bg-lime-300 p-5 compact:p-2 compact:text-sm transition-all hover:bg-slate-300">
@@ -14,7 +22,19 @@ export default function Page() {
       <div>
         <Button>helo</Button>
       </div>
-      <div></div>
+      <div>
+        <div className="w-56 relative group">
+          <input
+            type="text"
+            id="username"
+            required
+            className="w-full h-10 px-4 text-sm peer bg-gray-100 outline-none"
+          />
+          <label htmlFor="username" className={classes}>
+            Username
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
